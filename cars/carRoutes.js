9b926f00d15cd15331fa04b5846363ec65ec0292
Eddie Madrigal <edmadrigal@yahoo.com>
2020-04-14 16:47:11 -0700

    post feature added

--- cars/carRoutes.js
+++ cars/carRoutes.js
@@ -17,6 +17,19 @@ router.get('/', (req, res) => { // select * from cars
     });
 });
 
+router.post('/', (req, res) => {
+  const postData = req.body;
+  db('cars')
+  .insert( postData )
+  .then ( cars => {
+    res.status(200).json({ data: cars });
+  })
+  .catch( err => {
+    console.log( err );
+    res.status(500).json({ error: err.message });
+  });
+});
+
 router.get('/:id', (req, res) => {
   const id = req.params.id;
   res.status(200).send(`Hello from /GET /api/cars/${id} endpoint`);
